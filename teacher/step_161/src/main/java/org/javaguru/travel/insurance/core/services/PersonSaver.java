15272cc69d2a0b55a4aef6d501fd297e59473a99
javagurulv <javagurulv@gmail.com>
2024-05-18 10:44:29 +0300

    all steps

---
+++ teacher/step_161/src/main/java/org/javaguru/travel/insurance/core/services/PersonSaver.java
@@ -0,0 +1,35 @@
+package org.javaguru.travel.insurance.core.services;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.javaguru.travel.insurance.core.api.dto.PersonDTO;
+import org.javaguru.travel.insurance.core.domain.PersonEntity;
+import org.javaguru.travel.insurance.core.repositories.PersonEntityRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+class PersonSaver {
+
+    private final PersonEntityRepository repository;
+
+    PersonEntity savePerson(PersonDTO personDTO) {
+        Optional<PersonEntity> personOpt = repository.findBy(
+                personDTO.getPersonFirstName(),
+                personDTO.getPersonLastName(),
+                personDTO.getPersonCode());
+        if (personOpt.isPresent()) {
+            return personOpt.get();
+        } else {
+            PersonEntity person = new PersonEntity();
+            person.setFirstName(personDTO.getPersonFirstName());
+            person.setLastName(personDTO.getPersonLastName());
+            person.setPersonCode(personDTO.getPersonCode());
+            person.setBirthDate(personDTO.getPersonBirthDate());
+            return repository.save(person);
+        }
+    }
+
+}
